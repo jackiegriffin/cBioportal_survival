0a59d3594d6681b1f6ab0250d0afb3ee4770ec1a
lilfijj <jacqueline.r.griffin@dartmouth.edu>
2020-10-07 19:27:02 -0400

    Cox hazard ratio for AKR1C3 amplification in lobular breast cancer

--- AKR1C3_reg.R
+++ AKR1C3_reg.R
@@ -1,66 +1,57 @@
 
+#
+
 library("survival")
 library("survminer")
 library("plyr")
+library(forcats)
 
 # pull data from cbioportal
 cbio_OS_data <- read.delim(file = "KM_Plot__Overall_Survival_.txt")# 0=living 1=deceased
 cbio_AKR1C3amp_data <- read.delim(file = "alterations_across_samples.tsv")
-
-cnv <- read.delim(file = "cna.txt")
-
-
-names(cnv)[names(cnv)=="SAMPLE_ID"] <- "Sample.ID"
-colnames(cnv)
-colnames(merge_AKR1C3_OS)
 colnames(cbio_OS_data)
 colnames(cbio_AKR1C3amp_data)
-
 # merge data by patient ID
 merge_AKR1C3_OS <- merge(cbio_OS_data, cbio_AKR1C3amp_data, by = "Patient.ID")
+merge_AKR1C3_OS$OS_STATUS <- gsub(":.*","", merge_AKR1C3_OS$OS_STATUS) # remove everything after :
+# subset merged data
+colnames(merge_AKR1C3_OS)
+sub_merge_AKR1C3_OS <-merge_AKR1C3_OS[c(1,3:4,6:8)]
+colnames(sub_merge_AKR1C3_OS)
 
-merge_cnv <-merge(merge_AKR1C3_OS, cnv, by="Sample.ID")
-
-merge_cnv$OS_STATUS <- gsub(":.*","", merge_cnv$OS_STATUS) # remove everything after :
-sub_merge_cnv<-sub_merge_cnv[!(sub_merge_cnv$AKR1C3.x=="not profiled"),] # remove rows with AKR1C3 'not profiled'
-write.csv(sub_merge_cnv_omit, file = "s.csv")
-colnames(merge_cnv)
-
-# subset data
-sub_merge_cnv <-merge_cnv[c(1:8,13:14)]
-new <- read.csv(file = "s.csv", stringsAsFactors = FALSE, row.names = 1)
-colnames(sub_AKR1C3_OS)
-
-# need to remove data with NAs
-sub_merge_cnv_omit<-na.omit(sub_merge_cnv, cols = c("OS_STATUS", "OS_MONTHS"))
-
-str(sub_merge_cnv_omit)
-
-sub_merge_cnv_omit$OS_STATUS <- as.numeric(as.character(sub_merge_cnv_omit$OS_STATUS))
-sub_merge_cnv_omit$AKR1C3.y <- as.numeric(as.integer(sub_merge_cnv_omit$AKR1C3.y))
-
-
-# fit <- survfit(Surv(Days, status) ~ Tx, data = zr75_3b)
-
-Complete$AKR1C3 <- revalue(Complete$AKR1C3, c("C154F"="AMP"))
-
-coxph(formula = Surv(OS_MONTHS, OS_STATUS) ~ AKR1C3.x, data = new)
-
-# make vector of patient_ids
-colnames(AKR1C3)
+# change sructure
+str(sub_merge_AKR1C3_OS)
+sub_merge_AKR1C3_OS$OS_STATUS <- as.numeric(as.character(sub_merge_AKR1C3_OS$OS_STATUS))
+sub_merge_AKR1C3_OS$AKR1C3 <- as.character(as.factor(sub_merge_AKR1C3_OS$AKR1C3))
+str(sub_merge_AKR1C3_OS)
+# remove unwanted data
+sub_merge_AKR1C3_OS<-sub_merge_AKR1C3_OS[!(sub_merge_AKR1C3_OS$AKR1C3=="not profiled"),] # remove rows with AKR1C3 'not profiled'
+sub_merge_AKR1C3_OS<-na.omit(sub_merge_AKR1C3_OS, cols = c("OS_STATUS", "OS_MONTHS"))# remove rows without OS data
+sub_merge_AKR1C3_OS$AKR1C3 <- revalue(sub_merge_AKR1C3_OS$AKR1C3, c("C154F"="AMP"))
+# change AKR1C3 back to factor; want 2 factors
+str(sub_merge_AKR1C3_OS)
+sub_merge_AKR1C3_OS$AKR1C3 <- as.factor(as.character(sub_merge_AKR1C3_OS$AKR1C3))
+str(sub_merge_AKR1C3_OS)
+#flip levels, we want amp as 2 and no alteration as 1 
+sub_merge_AKR1C3_OS$AKR1C3 <- fct_rev(sub_merge_AKR1C3_OS$AKR1C3)
 
 
-patient_id <- reg_sub$Patient.ID
-AKR1C3 <- reg_sub[c(2:3)]
+# cox model
+#hazard ratio - positive value indicated greater risk (opposite of linear model)
+aka_cox <- coxph(formula = Surv(OS_MONTHS, OS_STATUS) ~ AKR1C3, data = sub_merge_AKR1C3_OS)
+beta_coef_i_want <- coef(summary(aka_cox))[1]
+hazard_ratio_i_want <- coef(summary(aka_cox))[2]
+p_value_i_want <- coef(summary(aka_cox))[5]
+#95% CI 1.58-6.50
 
 ## plot 
-sfit <- survfit(Surv(OS_MONTHS, OS_STATUS)~AKR1C3_HL, data=subset)
+sfit <- survfit(Surv(OS_MONTHS, OS_STATUS)~AKR1C3, data=sub_merge_AKR1C3_OS)
 plot(sfit)
-ggsurvplot(sfit, data = subset)
 ggsurvplot(sfit, conf.int=TRUE, pval=TRUE, risk.table=TRUE, 
-           legend.labs=c("Negative", "positive"), legend.title="AKR1C3 expression",  
-           palette=c("dodgerblue2", "orchid2"), 
-           title="Overall survival for AKR1C3 espression (METABRIC Breast Cancer)", 
-           risk.table.height=.15, data = subset)
+           legend.labs=c("No alteration", "Amplified"), legend.title="AKR1C3",  
+           palette=c("orchid2", "dodgerblue2"), 
+           title="AKR1C3 Amplification Risk in Lobular Breast Cancer", 
+           #risk.table.height=.15, 
+           data = sub_merge_AKR1C3_OS)
 
-
+head(sub_merge_AKR1C3_OS$AKR1C3)
